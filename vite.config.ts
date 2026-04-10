c60fd4a28fea0a9b7509373e58be02c706da2cc8
nozawana <hiharudaishi@gmail.com>
2026-04-10 01:36:23 +0000

    デプロイ設定

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// vite.config.ts
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: './', // または '/リポジトリ名/'
+  plugins: [react(), tailwindcss()],
 })
