014794417191d1a63d53bbf04f6a00e4caf65776
nozawana <hiharudaishi@gmail.com>
2026-04-10 02:18:07 +0000

    デプロイ設定

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './', // これを追加することで、どのURL階層でも正しく動作します
+  base: '/Create-a-codespace/', // これを追加することで、どのURL階層でも正しく動作します
   plugins: [
     react(),
     tailwindcss(),
