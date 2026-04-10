7423b338ba2a817aad0bf0f1aabcfc9ed45fb500
nozawana <hiharudaishi@gmail.com>
2026-04-10 01:55:49 +0000

    デプロイ設定

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-// vite.config.ts
 export default defineConfig({
-  base: './', // または '/リポジトリ名/'
-  plugins: [react(), tailwindcss()],
+  base: './', // これを追加することで、どのURL階層でも正しく動作します
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
