c156fffa2bea916b69c7768013de2b6bc8622fc0
nozawana <hiharudaishi@gmail.com>
2026-04-24 01:16:39 +0000

    デプロイ

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import { useState, useEffect } from "react";
 export default function App() {
   // ① 初期値：保存されているデータがあればそれを使い、なければ空配列 []
   const [todos, setTodos] = useState<string[]>(() => {
-    const saved = localStorage.getItem("wrong-key");
-    //const saved = localStorage.getItem("my-todos");
+    const saved = localStorage.getItem("my-todos");
     return saved ? JSON.parse(saved) : [];
   });
   const [text, setText] = useState<string>("");
 
   // ② 保存：todosの中身が変わるたびに実行される
   useEffect(() => {
-    localStorage.setItem("my-todos", JSON.stringify(todos));
+    localStorage.setItem("my-todos", todos);
+    //localStorage.setItem("my-todos", JSON.stringify(todos));
   }, [todos]);
 
   const addTodo = () => {
