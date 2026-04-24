e9733bb280837bbd3e1810cdc481d0eb42825b3f
nozawana <hiharudaishi@gmail.com>
2026-04-24 00:24:02 +0000

    デプロイ設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import './App.css'
-function App() {
+import { useState } from "react";
+
+export default function App() {
+  const [text, setText] = useState<string>("Hello");
+
   return (
-    <div className="bg-blue-700 text-white h-screen">
-      こんにちは
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">TODOアプリを作ろう</h1>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        className="border p-2 mr-2"
+      />
+      <p className="mt-4">入力中の文字: {text}</p>
     </div>
-  )
+  );
 }
-export default App
