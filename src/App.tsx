c0369dae283a67c91af8a892d543c3b00abd9487
nozawana <hiharudaishi@gmail.com>
2026-04-24 01:19:44 +0000

    デプロイ

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ export default function App() {
 
   // ② 保存：todosの中身が変わるたびに実行される
   useEffect(() => {
-    //localStorage.setItem("my-todos", todos);
     localStorage.setItem("my-todos", JSON.stringify(todos));
   }, [todos]);
 
